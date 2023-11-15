e5ee1acce093922e473bc2feb13f1af780d00403
B0HDAN1337 <machuliak1337@gmail.com>
2023-11-15 17:54:34 +0100

    git commit
    Dodanie struct dane_uzytkownika

--- Aplikacja_Biura/Aplikacja_Biura.cpp
+++ Aplikacja_Biura/Aplikacja_Biura.cpp
@@ -1,1 +1,13 @@
-﻿
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <regex>
+
+using namespace std;
+
+struct dane_uzytkownika {
+    string nazwa;
+    string haslo;
+    string email;
+};
