b22153e2ca64a391b2f49a6c1168e7d2ee13c1d0
B0HDAN1337 <machuliak1337@gmail.com>
2023-11-15 18:13:25 +0100

    Menu
    Dodanie Menu, Dodanie dostępnych ofert

--- Aplikacja_Biura/Aplikacja_Biura.cpp
+++ Aplikacja_Biura/Aplikacja_Biura.cpp
@@ -65,3 +65,234 @@ public:
         pobieranie_danych();
     }
 };
+
+class po_zalogowaniu : public Zarzadanie_uzytkownikami {
+public:
+    dane_uzytkownika aktualny_uzytkownik;
+    void funkcje(string nazwa) {
+        string nazwa1 = nazwa;
+        cout << "Co chcesz zrobic teraz?" << endl;
+        cout << "1 - Sprawdz dostepne oferty!" << endl;
+        cout << "2 - Sprawdz moj profil!" << endl;
+        cout << "3 - Wyloguj sie!" << endl;
+
+        int liczba;
+        cin >> liczba;
+
+        switch (liczba)
+        {
+        case 1:
+            wyswietlOferty();
+            break;
+        case 2:
+            sprawdzProfil();
+            break;
+        case 3:
+            cout << "Wylogowano" << endl;
+            break;
+        default:
+            cout << "Nie ma takiej opcji! Spróbuj jeszcze raz!" << endl;
+            break;
+        }
+    }
+
+
+    void wyswietlOferty() {
+        int kraj;
+        do {
+            cout << "Wybierz kraj!" << endl;
+            cout << "1 - Grecja" << endl;
+            cout << "2 - Hiszpania" << endl;
+            cout << "3 - Turcja" << endl;
+            cout << "4 - Włochy" << endl;
+            cout << "5 - Egipt" << endl;
+            cout << "0 - Wyjdz z listy panstw" << endl;
+
+
+            cin >> kraj;
+
+            string kraj1;
+            switch (kraj) {
+            case 1:
+                kraj1 = "Grecja";
+                cout << "Wybrany kraj to: " << kraj1 << endl;
+                break;
+            case 2:
+                kraj1 = "Hiszpania";
+                cout << "Wybrany kraj to: " << kraj1 << endl;
+                break;
+            case 3:
+                kraj1 = "Turcja";
+                cout << "Wybrany kraj to: " << kraj1 << endl;
+                break;
+            case 4:
+                kraj1 = "Włochy";
+                cout << "Wybrany kraj to: " << kraj1 << endl;
+                break;
+            case 5:
+                kraj1 = "Egipt";
+                cout << "Wybrany kraj to: " << kraj1 << endl;
+                break;
+            case 0:
+                break;
+            default:
+                cout << "Nie ma takiej opcji!" << endl;
+                break;
+            }
+
+            switch (kraj) {
+            case 1:
+                wyswietlOfertyGrecja();
+                break;
+            case 2:
+                wyswietlOfertyHiszpania();
+                break;
+            case 3:
+                wyswietlOfertyTurcja();
+                break;
+            case 4:
+                wyswietlOfertyWlochy();
+                break;
+            case 5:
+                wyswietlOfertyEgipt();
+                break;
+            default:
+                return;
+            }
+        } while (kraj != 0);
+    }
+
+    void wyswietlOfertyGrecja() {
+        cout << " " << endl;
+        cout << "-----------------Oferta 1----------------" << endl;
+        cout << "Kraj: Grecja" << endl;
+        cout << "Liczba dni: 14" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 3200 zł" << endl;
+        cout << "Data: 13.07.2024 - 27.07.2024" << endl;
+        cout << "Miejscowość: Ateny" << endl;
+        cout << " " << endl;
+        cout << "-----------------Oferta 2----------------" << endl;
+        cout << "Kraj: Grecja" << endl;
+        cout << "Liczba dni: 10" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 2900 zł" << endl;
+        cout << "Data: 15.08.2024 - 25.08.2024" << endl;
+        cout << "Miejscowość: Kreta" << endl;
+        cout << " " << endl;
+        cout << "-----------------Oferta 3----------------" << endl;
+        cout << "Kraj: Grecja" << endl;
+        cout << "Liczba dni: 7" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 2300 zł" << endl;
+        cout << "Data: 06.06.2024 - 13.06.2024" << endl;
+        cout << "Miejscowość: Saloniki" << endl;
+    }
+    void wyswietlOfertyHiszpania() {
+        cout << " " << endl;
+        cout << "-----------------Oferta 1----------------" << endl;
+        cout << "Kraj: Hiszpania" << endl;
+        cout << "Liczba dni: 14" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 3200 zł" << endl;
+        cout << "Data: 13.07.2024 - 27.07.2024" << endl;
+        cout << "Miejscowość: Madrid" << endl;
+        cout << " " << endl;
+        cout << "-----------------Oferta 2----------------" << endl;
+        cout << "Kraj: Hiszpania" << endl;
+        cout << "Liczba dni: 10" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 2900 zł" << endl;
+        cout << "Data: 15.08.2024 - 25.08.2024" << endl;
+        cout << "Miejscowość: Barcelona" << endl;
+        cout << " " << endl;
+        cout << "-----------------Oferta 3----------------" << endl;
+        cout << "Kraj: Hiszpania" << endl;
+        cout << "Liczba dni: 7" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 2300 zł" << endl;
+        cout << "Data: 06.06.2024 - 13.06.2024" << endl;
+        cout << "Miejscowość: Vigo" << endl;
+    }
+    void wyswietlOfertyTurcja() {
+        cout << " " << endl;
+        cout << "-----------------Oferta 1----------------" << endl;
+        cout << "Kraj: Turcja" << endl;
+        cout << "Liczba dni: 14" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 3200 zł" << endl;
+        cout << "Data: 13.07.2024 - 27.07.2024" << endl;
+        cout << "Miejscowość: Istambuł" << endl;
+        cout << " " << endl;
+        cout << "-----------------Oferta 2----------------" << endl;
+        cout << "Kraj: Turcja" << endl;
+        cout << "Liczba dni: 10" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 2900 zł" << endl;
+        cout << "Data: 15.08.2024 - 25.08.2024" << endl;
+        cout << "Miejscowość: Antalya" << endl;
+        cout << " " << endl;
+        cout << "-----------------Oferta 3----------------" << endl;
+        cout << "Kraj: Turcja" << endl;
+        cout << "Liczba dni: 7" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 2300 zł" << endl;
+        cout << "Data: 06.06.2024 - 13.06.2024" << endl;
+        cout << "Miejscowość: Bodrum" << endl;
+    }
+    void wyswietlOfertyWlochy() {
+        cout << " " << endl;
+        cout << "-----------------Oferta 1----------------" << endl;
+        cout << "Kraj: Wlochy" << endl;
+        cout << "Liczba dni: 14" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 3200 zł" << endl;
+        cout << "Data: 13.07.2024 - 27.07.2024" << endl;
+        cout << "Miejscowość: Rzym" << endl;
+        cout << " " << endl;
+        cout << "-----------------Oferta 2----------------" << endl;
+        cout << "Kraj: Wlochy" << endl;
+        cout << "Liczba dni: 10" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 2900 zł" << endl;
+        cout << "Data: 15.08.2024 - 25.08.2024" << endl;
+        cout << "Miejscowość: Florencja" << endl;
+        cout << " " << endl;
+        cout << "-----------------Oferta 3----------------" << endl;
+        cout << "Kraj: Wlochy" << endl;
+        cout << "Liczba dni: 7" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 2300 zł" << endl;
+        cout << "Data: 06.06.2024 - 13.06.2024" << endl;
+        cout << "Miejscowość: Wenecja" << endl;
+    }
+    void wyswietlOfertyEgipt() {
+        cout << " " << endl;
+        cout << "-----------------Oferta 1----------------" << endl;
+        cout << "Kraj: Egipt" << endl;
+        cout << "Liczba dni: 14" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 3200 zł" << endl;
+        cout << "Data: 13.07.2024 - 27.07.2024" << endl;
+        cout << "Miejscowość: Kair" << endl;
+        cout << " " << endl;
+        cout << "-----------------Oferta 2----------------" << endl;
+        cout << "Kraj: Egipt" << endl;
+        cout << "Liczba dni: 10" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 2900 zł" << endl;
+        cout << "Data: 15.08.2024 - 25.08.2024" << endl;
+        cout << "Miejscowość: Hurghada" << endl;
+        cout << " " << endl;
+        cout << "-----------------Oferta 3----------------" << endl;
+        cout << "Kraj: Egipt" << endl;
+        cout << "Liczba dni: 7" << endl;
+        cout << "Liczba osób: 2" << endl;
+        cout << "Cena: 2300 zł" << endl;
+        cout << "Data: 06.06.2024 - 13.06.2024" << endl;
+        cout << "Miejscowość: Luxor" << endl;
+    }
+    void sprawdzProfil() {
+
+    }
+};
